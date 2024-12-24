1f1357f8fdca90a3ab336c4dbd6eda2af82a0c6e
taiwbi <admin@taiwbi.com>
2024-12-24 09:17:55 +0330

    fix: Fixed 'called array.toString() on a Uint8Array instance'

--- extension.ts
+++ extension.ts
@@ -111,7 +111,7 @@ export default class AccentColorIconThemeExtension extends Extension {
       const versionFilePath = `${themeDir}/version`;
       if (GLib.file_test(versionFilePath, GLib.FileTest.EXISTS)) {
         const [success, contents] = GLib.file_get_contents(versionFilePath);
-        if (success && parseInt(contents.toString()) == 4) {
+        if (success && parseInt(new TextDecoder().decode(contents)) == 4) {
           if (!changeAppIcons) {
             return;
           } else if (GLib.file_test(themeDir + "/scalable/apps/org.gnome.Nautilus-symbolic.svg", GLib.FileTest.EXISTS)) {
